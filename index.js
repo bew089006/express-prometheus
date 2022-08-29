5704b32f1cfc35711dc7713a6ee9769eb7b63ea4
Wattanan <bew089006@gmail.com>
2022-08-29 21:59:08 +0700

    dockerize docker-compose

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import express from "express";
+import express from 'express';
 
 const APP_PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.status(200).send({
-    message: "Hello world",
-  });
+app.get('/', (req, res, next) => {
+	res.status(200).send({
+		message: 'Hello world',
+	});
 });
 
 app.listen(APP_PORT, () => {
-  console.log("Application starting at port " + APP_PORT);
+	console.log('Test read ENV CHECK = ', process.env.CHECK);
+	console.log('Application starting at port ' + APP_PORT);
 });
 
 export default app;
